d64ce5c3943bc6386d8b1b42877dc5f358d94b7f
shubham sarode <intern.shubham@sangnak.in>
2022-04-13 09:41:56 +0530

    uses useCallback

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,36 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import API from "../API";
 
 export const useMovieFetch = (movieId) => {
   const [state, setState] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const fetchMovie = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(false);
+      const movie = await API.fetchMovie(movieId);
+      const credits = await API.fetchCredits(movieId);
+      //get directors
+      const directors = credits.crew.filter(
+        (member) => member.job === "Director"
+      );
+
+      setState({
+        ...movie,
+        actors: credits.cast,
+        directors,
+      });
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
+  }, [movieId]);
 
   useEffect(() => {
-    const fetchMovie = async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const movie = await API.fetchMovie(movieId);
-        const credits = await API.fetchCredits(movieId);
-        //get directors
-        const directors = credits.crew.filter(
-          (member) => member.job === "Director"
-        );
-
-        setState({
-          ...movie,
-          actors: credits.cast,
-          directors,
-        });
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-      }
-    };
-
     fetchMovie();
-  }, [movieId]);
+  }, [movieId, fetchMovie]);
 
   return { state, loading, error };
 };
